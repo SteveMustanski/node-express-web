993d2f5b336cadc9201de0d0b1c396c2afd1c395
SteveMustanski <steve.mustanski@gmail.com>
2019-05-01 15:34:51 -0400

    added logic to check for / path and provided 404 logic

--- webserver/pureNodeServer.js
+++ webserver/pureNodeServer.js
@@ -1,9 +1,18 @@
 const http = require('http');
+const url = require('url');
 
 function handler(req, res) {
-  res.writeHead(200, { 'Content-type': 'text/plain' });
-  res.write('Hello, this is the web server');
-  res.end();
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/') {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.write('Hello, this is the web server');
+    res.end();
+  } else {
+    res.writeHead(404, { 'Content-type': 'text/plain' });
+    res.write('404, page not found!');
+    res.end();
+  }
 }
 
 const server = http.createServer(handler);
