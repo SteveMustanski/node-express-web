c1b0c4df4f0eceb27b87a2335428ae6210b21362
SteveMustanski <steve.mustanski@gmail.com>
2019-05-01 16:08:43 -0400

    added examples of creating errors

--- webserver/index.js
+++ webserver/index.js
@@ -5,9 +5,19 @@ const app = express();
 app.get('/', (req, res, next) => {
   return res.send('Hello, I am an expressified websever');
 });
+
 app.get('/time', (req, res, next) => {
   return res.send(new Date().toString());
 });
+
+app.get('/throw', (req, res, next) => {
+  throw new Error('Something is wrong');
+});
+
+app.get('/next', (req, res, next) => {
+  return next(new Error('Something is wrong'));
+});
+
 app.get('/hello:name', (req, res, next) => {
   if (!req.query.name) {
     return res.status(400).end();
