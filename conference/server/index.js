a10733adfa8fa116dab6b8cfd3e9ef43c65ff2d7
SteveMustanski <steve.mustanski@gmail.com>
2019-05-02 12:44:54 -0400

    moved provied static files to public and added static route to application

--- conference/server/index.js
+++ conference/server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
+app.use(express.static('public'));
 app.use('/', routes());
 
 app.listen(3000, () => {
