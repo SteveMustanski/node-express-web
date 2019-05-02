38458133aa23ff5691143331d75d47d254075013
SteveMustanski <steve.mustanski@gmail.com>
2019-05-02 12:47:48 -0400

    added 204 response for favicon.ico

--- conference/server/index.js
+++ conference/server/index.js
@@ -4,6 +4,10 @@ const app = express();
 const routes = require('./routes');
 
 app.use(express.static('public'));
+// tell browser/express to not worry about favicon
+app.get('/favicon.ico', (req, res, next) => {
+  return res.sendStatus(204);
+});
 app.use('/', routes());
 
 app.listen(3000, () => {
