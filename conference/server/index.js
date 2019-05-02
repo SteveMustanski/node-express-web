22a050d941c1c167cf81f5a6e67627af61d1d8fc
SteveMustanski <steve.mustanski@gmail.com>
2019-05-02 13:02:12 -0400

    added pug and draft index pug template

--- conference/server/index.js
+++ conference/server/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const routes = require('./routes');
+
+// setup pug and point to where the templates are located
+// also show non-compressed html in dev for debugging
+app.set('view engine', 'pug');
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static('public'));
 // tell browser/express to not worry about favicon
