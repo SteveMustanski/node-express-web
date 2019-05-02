c2c3637bd9ef90156467d705519dee188c36e3b4
SteveMustanski <steve.mustanski@gmail.com>
2019-05-02 12:31:32 -0400

    added draft route to '/'

---
+++ conference/server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const routes = require('./routes');
+
+app.use('/', routes());
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
+
+module.exports = app;
